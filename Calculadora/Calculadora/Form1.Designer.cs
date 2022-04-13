a144b7295c0fb74d1d91e599f551f8a3cf348aba
Samuel e Davie <cl201278@unicamp.br>
2022-04-13 16:50:30 -0300

    Implementação do Background da Calculadora

--- Calculadora/Calculadora/Form1.Designer.cs
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -354,7 +354,7 @@ namespace Calculadora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(377, 430);
             this.Controls.Add(this.btnDois);
             this.Controls.Add(this.btnOito);
