6003681da838741d8627275d84949ea882262af6
Davie <cl201275@g.unicamp.br>
2022-08-31 17:27:32 -0300

    ..
    ..

---
+++ Aula11_Exemplo01/Aula11_Exemplo01/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace Aula11_Exemplo01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEvento = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mskData = new System.Windows.Forms.MaskedTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBonoro = new System.Windows.Forms.TextBox();
+            this.txtTebet = new System.Windows.Forms.TextBox();
+            this.txtLula = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(333, 379);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBox2);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.mskData);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtEvento);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(325, 353);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Datas";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(325, 353);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Votação";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Evento:";
+            // 
+            // txtEvento
+            // 
+            this.txtEvento.Location = new System.Drawing.Point(56, 19);
+            this.txtEvento.Name = "txtEvento";
+            this.txtEvento.Size = new System.Drawing.Size(246, 20);
+            this.txtEvento.TabIndex = 1;
+            this.txtEvento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEvento_KeyPress);
+            this.txtEvento.Leave += new System.EventHandler(this.txtEvento_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data:";
+            // 
+            // mskData
+            // 
+            this.mskData.Location = new System.Drawing.Point(56, 55);
+            this.mskData.Mask = "00/00/0000";
+            this.mskData.Name = "mskData";
+            this.mskData.Size = new System.Drawing.Size(129, 20);
+            this.mskData.TabIndex = 3;
+            this.mskData.ValidatingType = typeof(System.DateTime);
+            this.mskData.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.mskData_KeyPress);
+            this.mskData.Leave += new System.EventHandler(this.mskData_Leave);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(194, 225);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listBox1_KeyPress);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(209, 115);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(102, 225);
+            this.listBox2.TabIndex = 5;
+            this.listBox2.Click += new System.EventHandler(this.listBox2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.listBox3);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 177);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Candidatos";
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Items.AddRange(new object[] {
+            "Bolsonaro",
+            "Simone Tebet",
+            "Lula "});
+            this.listBox3.Location = new System.Drawing.Point(6, 34);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(278, 64);
+            this.listBox3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Votar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtLula);
+            this.groupBox2.Controls.Add(this.txtTebet);
+            this.groupBox2.Controls.Add(this.txtBonoro);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 217);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(296, 125);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Bolsonaro:";
+            this.label4.Click += new System.EventHandler(this.Bolsonaro_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Simone Tebet:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Lula:";
+            // 
+            // txtBonoro
+            // 
+            this.txtBonoro.Location = new System.Drawing.Point(99, 29);
+            this.txtBonoro.Name = "txtBonoro";
+            this.txtBonoro.Size = new System.Drawing.Size(144, 20);
+            this.txtBonoro.TabIndex = 5;
+            this.txtBonoro.Text = "0";
+            // 
+            // txtTebet
+            // 
+            this.txtTebet.Location = new System.Drawing.Point(99, 57);
+            this.txtTebet.Name = "txtTebet";
+            this.txtTebet.Size = new System.Drawing.Size(144, 20);
+            this.txtTebet.TabIndex = 6;
+            this.txtTebet.Text = "0";
+            // 
+            // txtLula
+            // 
+            this.txtLula.Location = new System.Drawing.Point(99, 88);
+            this.txtLula.Name = "txtLula";
+            this.txtLula.Size = new System.Drawing.Size(144, 20);
+            this.txtLula.TabIndex = 7;
+            this.txtLula.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(301, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 398);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Exemplo 01";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MaskedTextBox mskData;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEvento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox txtLula;
+        private System.Windows.Forms.TextBox txtTebet;
+        private System.Windows.Forms.TextBox txtBonoro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Aula11_Exemplo01/Aula11_Exemplo01/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula11_Exemplo01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void txtEvento_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (txtEvento.Text != string.Empty)
+                    mskData.Focus();
+            }
+        }
+
+        private void mskData_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                try
+                {
+                    Convert.ToDateTime(mskData.Text);
+                    listBox1.Items.Add(txtEvento.Text);
+                    listBox2.Items.Add(mskData.Text);
+                    txtEvento.Clear();
+                    mskData.Clear();
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("Data inválida.");
+                    mskData.Focus();
+                }
+            }
+        }
+
+        private void Bolsonaro_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox3.SelectedIndex == 0)
+                txtBonoro.Text = (int.Parse(txtBonoro.Text) + 1).ToString();
+            else if (listBox3.SelectedIndex == 1)
+                txtTebet.Text = (int.Parse(txtTebet.Text) + 1).ToString();
+            else if (listBox3.SelectedIndex == 2)
+                txtLula.Text = (int.Parse(txtLula.Text) + 1).ToString();
+            else
+                MessageBox.Show("Selecione um candidato!");
+
+            listBox3.ClearSelected();
+        }
+
+        private void txtEvento_Leave(object sender, EventArgs e)
+        {
+            if (txtEvento.Text.Equals(""))
+                txtEvento.Focus();
+        }
+
+        private void mskData_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtBonoro.Text = "0";
+            txtTebet.Text = "0";
+            txtLula.Text = "0";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox2.Items.RemoveAt(listBox1.SelectedIndex);
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Selecione um evento");
+            }
+        }
+
+        private void listBox2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+                listBox1.Items.RemoveAt(listBox2.SelectedIndex);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Selecione um evento");
+            }
+        }
+    }
+}
